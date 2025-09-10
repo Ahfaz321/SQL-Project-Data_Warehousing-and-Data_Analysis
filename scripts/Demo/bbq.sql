6a47739356c1f8a6162fdd70dc22ccf3cb3b1c27
Ahfaz321 <Ahfaz.ahs@gmail.com>
2025-09-10 23:40:47 +0530

    Added Select Command For all Bronze Schema tables

---
+++ scripts/Demo/bbq.sql
@@ -0,0 +1,11 @@
+SELECT * FROM bronze.crm_sales_details
+
+SELECT * FROM bronze.crm_cust_id
+
+SELECT * FROM bronze.crm_prd_info
+
+SELECT * FROM bronze.crm_cust_info
+
+SELECT * FROM bronze.erp_cust_az12
+
+SELECT * FROM bronze.erp_px_cat_g1v2
